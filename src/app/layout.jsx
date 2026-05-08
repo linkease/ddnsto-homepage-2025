768edb5c273b171247af7589188e0175800d5862
MickeySir <2471021392@qq.com>
2026-05-08 02:56:04 +0000

    添加谷歌分析

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="zh-CN">
+      <head>
+        <script src="/script/analytics.js"></script>
+        <script src="/script/google-analytics.js"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
